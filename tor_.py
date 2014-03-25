97c562fd28dbd6952b3a3775550e6689635d968c
martin@seraph <mweinelt@gmail.com>
2014-03-25 13:13:08 +0100

    Initial Commits

---
+++ tor_.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python
+import sys
+import __main__
+try:
+    from stem.control import Controller
+except ImportError:
+    print('no (tor-munin requires the stem library from https://stem.torproject.org.)')
+    sys.exit(0)
+
+# configuration
+port = 9051
+
+'''
+ Here be dragons
+'''
+
+#%# family=auto
+#%# capabilities=autoconf suggest
+
+
+#########################
+# Base Class
+#########################
+
+class TorPlugin:
+    def __init__(self):
+        raise NotImplementedError
+
+    def conf(self):
+        raise NotImplementedError
+
+    @staticmethod
+    def conf_from_dict(graph, labels):
+        # header
+        for key, val in graph.iteritems():
+            print('graph_{} {}'.format(key, val))
+        # values
+        for label, attributes in labels.iteritems():
+            for key, val in attributes.iteritems():
+                print('{}.{} {}'.format(label, key, val))
+
+    @staticmethod
+    def autoconf():
+        try:
+            with Controller.from_port(port=port) as controller:
+                controller.authenticate()
+                if controller.is_authenticated():
+                    print('yes')
+                else:
+                    print('no (Authentication failed)')
+        except:
+            print('no (Connection failed)')
+
+    @staticmethod
+    def suggest():
+        options = ['connections']
+        for option in options:
+            print(option)
+
+    def fetch(self):
+        raise NotImplementedError
+
+
+##########################
+# Child Classes
+##########################
+
+class TorConnections(TorPlugin):
+    def __init__(self):
+        pass
+
+    def conf(self):
+        graph = {'title': 'Tor Connections',
+                 'args': '-l 0 --base 1000',
+                 'vlabel': 'connections',
+                 'category': 'Tor',
+                 'info': 'OR connections by state'}
+        labels = {'new': {'label': 'new', 'min': 0, 'max': 25000, 'type': 'GAUGE'},
+                  'launched': {'label': 'launched', 'min': 0, 'max': 25000, 'type': 'GAUGE'},
+                  'connected': {'label': 'connected', 'min': 0, 'max': 25000, 'type': 'GAUGE'},
+                  'failed': {'label': 'failed', 'min': 0, 'max': 25000, 'type': 'GAUGE'},
+                  'closed': {'label': 'closed', 'min': 0, 'max': 25000, 'type': 'GAUGE'}}
+
+        TorPlugin.conf_from_dict(graph, labels)
+
+    def fetch(self):
+        with Controller.from_port(port=9051) as controller:
+            controller.authenticate()
+
+            response = controller.get_info('orconn-status')
+            connections = response.split('\n')
+
+            states = {'NEW': 0, 'LAUNCHED': 0, 'CONNECTED': 0, 'FAILED': 0, 'CLOSED': 0}
+            for connection in connections:
+                states[connection.rsplit(None, 1)[-1]] += 1
+            for state, count in states.iteritems():
+                print('{}.value {}'.format(state.lower(), count))
+
+
+if __name__ == '__main__':
+    param = None
+    if len(sys.argv) > 1:
+        param = sys.argv[1].lower()
+    else:
+        param = 'fetch'
+
+    if param == 'autoconf':
+        TorPlugin.autoconf()
+        sys.exit(0)
+    elif param == 'suggest':
+        TorPlugin.suggest()
+        sys.exit(0)
+    else:
+        # detect data provider
+        provider = None
+        if __main__.__file__.endswith('_connections'):
+            provider = TorConnections()
+        else:
+            print('Unknown plugin name, try "suggest" for a list of possible ones.')
+            sys.exit(0)
+
+        if param == 'config':
+            provider.conf()
+        elif param == 'fetch':
+            provider.fetch()
+        else:
+            print('Unknown parameter "{}"'.format(param))
