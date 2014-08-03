fe3f4e049aa33f1de06f05d8b22a6f3863209f75
Federico Ceratto <federico.ceratto@gmail.com>
2014-08-03 19:23:30 +0100

    PEP8: update style

--- get_info.py
+++ get_info.py
@@ -3,6 +3,7 @@ import stem
 import stem.control
 import argparse
 
+
 def get_info(query, port=9051):
     with stem.control.Controller.from_port(port=port) as controller:
         controller.authenticate()
@@ -15,11 +16,11 @@ def get_info(query, port=9051):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("query", type=str, help='query parameter for get_info request')
-    parser.add_argument("-p", "--port", type=int, help='set the port used for connecting to tor (default: 9051)', default=9051)
+    parser.add_argument("query", type=str,
+                        help='query parameter for get_info request')
+    parser.add_argument("-p", "--port", type=int,
+                        help='set the port used for connecting to tor'
+                        ' (default: 9051)', default=9051)
     args = parser.parse_args()
 
     get_info(args.query, args.port)
-
-
-
