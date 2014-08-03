89dd246ee6ffd62123abaa7d39ee93aecbdc0574
Federico Ceratto <federico.ceratto@gmail.com>
2014-08-03 19:18:51 +0100

    PEP8: replace tabs

--- get_info.py
+++ get_info.py
@@ -4,22 +4,22 @@ import stem.control
 import argparse
 
 def get_info(query, port=9051):
-	with stem.control.Controller.from_port(port=port) as controller:
-		controller.authenticate()
+    with stem.control.Controller.from_port(port=port) as controller:
+        controller.authenticate()
 
-		try:
-			response = controller.get_info(query)
-			print(response)
-		except stem.InvalidArguments as e:
-			print("{}".format(e))
+        try:
+            response = controller.get_info(query)
+            print(response)
+        except stem.InvalidArguments as e:
+            print("{}".format(e))
 
 if __name__ == "__main__":
-	parser = argparse.ArgumentParser()
-	parser.add_argument("query", type=str, help='query parameter for get_info request')
-	parser.add_argument("-p", "--port", type=int, help='set the port used for connecting to tor (default: 9051)', default=9051)
-	args = parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("query", type=str, help='query parameter for get_info request')
+    parser.add_argument("-p", "--port", type=int, help='set the port used for connecting to tor (default: 9051)', default=9051)
+    args = parser.parse_args()
 
-	get_info(args.query, args.port)
+    get_info(args.query, args.port)
 
-	
-		
+
+
--- tor_.py
+++ tor_.py
@@ -2,11 +2,11 @@
 from __future__ import print_function
 import sys
 try:
-	import stem
-	from stem.control import Controller
+    import stem
+    from stem.control import Controller
 except ImportError:
-	print('no (tor-munin requires the stem library from https://stem.torproject.org.)')
-	sys.exit(0)
+    print('no (tor-munin requires the stem library from https://stem.torproject.org.)')
+    sys.exit(0)
 
 # configuration
 port = 9051
@@ -23,42 +23,42 @@ port = 9051
 #########################
 
 class TorPlugin:
-	def __init__(self):
-		raise NotImplementedError
+    def __init__(self):
+        raise NotImplementedError
 
-	def conf(self):
-		raise NotImplementedError
+    def conf(self):
+        raise NotImplementedError
 
-	@staticmethod
-	def conf_from_dict(graph, labels):
-		# header
-		for key, val in graph.iteritems():
-			print('graph_{} {}'.format(key, val))
-		# values
-		for label, attributes in labels.iteritems():
-			for key, val in attributes.iteritems():
-				print('{}.{} {}'.format(label, key, val))
+    @staticmethod
+    def conf_from_dict(graph, labels):
+        # header
+        for key, val in graph.iteritems():
+            print('graph_{} {}'.format(key, val))
+        # values
+        for label, attributes in labels.iteritems():
+            for key, val in attributes.iteritems():
+                print('{}.{} {}'.format(label, key, val))
 
-	@staticmethod
-	def autoconf():
-		try:
-			with Controller.from_port(port=port) as controller:
-				try:
-					controller.authenticate()
-					print('yes')
-				except stem.connection.AuthenticationFailure as e:
-					print('no (Authentication failed: {})'.format(e))
-		except stem.connection:
-			print('no (Connection failed)')
+    @staticmethod
+    def autoconf():
+        try:
+            with Controller.from_port(port=port) as controller:
+                try:
+                    controller.authenticate()
+                    print('yes')
+                except stem.connection.AuthenticationFailure as e:
+                    print('no (Authentication failed: {})'.format(e))
+        except stem.connection:
+            print('no (Connection failed)')
 
-	@staticmethod
-	def suggest():
-		options = ['connections', 'traffic']
-		for option in options:
-			print(option)
+    @staticmethod
+    def suggest():
+        options = ['connections', 'traffic']
+        for option in options:
+            print(option)
 
-	def fetch(self):
-		raise NotImplementedError
+    def fetch(self):
+        raise NotImplementedError
 
 
 ##########################
@@ -66,106 +66,106 @@ class TorPlugin:
 ##########################
 
 class TorConnections(TorPlugin):
-	def __init__(self):
-		pass
+    def __init__(self):
+        pass
 
-	def conf(self):
-		graph = {'title': 'Connections',
-				 'args': '-l 0 --base 1000',
-				 'vlabel': 'connections',
-				 'category': 'Tor',
-				 'info': 'OR connections by state'}
-		labels = {'new': {'label': 'new', 'min': 0, 'max': 25000, 'type': 'GAUGE'},
-				  'launched': {'label': 'launched', 'min': 0, 'max': 25000, 'type': 'GAUGE'},
-				  'connected': {'label': 'connected', 'min': 0, 'max': 25000, 'type': 'GAUGE'},
-				  'failed': {'label': 'failed', 'min': 0, 'max': 25000, 'type': 'GAUGE'},
-				  'closed': {'label': 'closed', 'min': 0, 'max': 25000, 'type': 'GAUGE'}}
+    def conf(self):
+        graph = {'title': 'Connections',
+                 'args': '-l 0 --base 1000',
+                 'vlabel': 'connections',
+                 'category': 'Tor',
+                 'info': 'OR connections by state'}
+        labels = {'new': {'label': 'new', 'min': 0, 'max': 25000, 'type': 'GAUGE'},
+                  'launched': {'label': 'launched', 'min': 0, 'max': 25000, 'type': 'GAUGE'},
+                  'connected': {'label': 'connected', 'min': 0, 'max': 25000, 'type': 'GAUGE'},
+                  'failed': {'label': 'failed', 'min': 0, 'max': 25000, 'type': 'GAUGE'},
+                  'closed': {'label': 'closed', 'min': 0, 'max': 25000, 'type': 'GAUGE'}}
 
-		TorPlugin.conf_from_dict(graph, labels)
+        TorPlugin.conf_from_dict(graph, labels)
 
-	def fetch(self):
-		with Controller.from_port(port=port) as controller:
-			try:
-				controller.authenticate()
+    def fetch(self):
+        with Controller.from_port(port=port) as controller:
+            try:
+                controller.authenticate()
 
-				response = controller.get_info('orconn-status', None)
-				if response == None:
-					print("No response from Tor Daemon in TorConnection.fetch()", file=sys.stderr)
-					sys.exit(-1)
-				else:
-					connections = response.split('\n')
-					states = dict((state, 0) for state in stem.ORStatus)
-					for connection in connections:
-						states[connection.rsplit(None, 1)[-1]] += 1
-					for state, count in states.iteritems():
-						print('{}.value {}'.format(state.lower(), count))
-			except stem.connection.AuthenticationFailure as e:
-				print('Authencation failed ({})'.format(e))
+                response = controller.get_info('orconn-status', None)
+                if response == None:
+                    print("No response from Tor Daemon in TorConnection.fetch()", file=sys.stderr)
+                    sys.exit(-1)
+                else:
+                    connections = response.split('\n')
+                    states = dict((state, 0) for state in stem.ORStatus)
+                    for connection in connections:
+                        states[connection.rsplit(None, 1)[-1]] += 1
+                    for state, count in states.iteritems():
+                        print('{}.value {}'.format(state.lower(), count))
+            except stem.connection.AuthenticationFailure as e:
+                print('Authencation failed ({})'.format(e))
 
 class TorTraffic(TorPlugin):
-	def __init__(self):
-		pass
+    def __init__(self):
+        pass
 
-	def conf(self):
-		graph = {'title': 'Traffic',
-				 'args': '-l 0 --base 1024',
-				 'vlabel': 'data',
-				 'category': 'Tor',
-				 'type': 'COUNTER',
-				 'info': 'bytes read/written'}
-		labels = {'read': {'label': 'read', 'min': 0, 'type': 'COUNTER'},
-				  'written': {'label': 'written', 'min': 0, 'type': 'COUNTER'}}
+    def conf(self):
+        graph = {'title': 'Traffic',
+                 'args': '-l 0 --base 1024',
+                 'vlabel': 'data',
+                 'category': 'Tor',
+                 'type': 'COUNTER',
+                 'info': 'bytes read/written'}
+        labels = {'read': {'label': 'read', 'min': 0, 'type': 'COUNTER'},
+                  'written': {'label': 'written', 'min': 0, 'type': 'COUNTER'}}
 
-		TorPlugin.conf_from_dict(graph, labels)
+        TorPlugin.conf_from_dict(graph, labels)
 
-	def fetch(self):
-		with Controller.from_port(port=port) as controller:
-			try:
-				controller.authenticate()
+    def fetch(self):
+        with Controller.from_port(port=port) as controller:
+            try:
+                controller.authenticate()
 
-				response = controller.get_info('traffic/read', None)
-				if response == None:
-					print("Error while reading traffic/read from Tor Deamon", file=sys.stderr)
-					sys.exit(-1)
+                response = controller.get_info('traffic/read', None)
+                if response == None:
+                    print("Error while reading traffic/read from Tor Deamon", file=sys.stderr)
+                    sys.exit(-1)
 
-				print('read.value {}'.format(response))
+                print('read.value {}'.format(response))
 
-				response = controller.get_info('traffic/written', None)
-				if response == None:
-					print("Error while reading traffic/write from Tor Deamon", file=sys.stderr)
-					sys.exit(-1)
-				print('written.value {}'.format(response))
-			except stem.connection.AuthenticationFailure:
-				print('Authentcation failed ({})'.format(e))
+                response = controller.get_info('traffic/written', None)
+                if response == None:
+                    print("Error while reading traffic/write from Tor Deamon", file=sys.stderr)
+                    sys.exit(-1)
+                print('written.value {}'.format(response))
+            except stem.connection.AuthenticationFailure:
+                print('Authentcation failed ({})'.format(e))
 
 
 if __name__ == '__main__':
-	param = None
-	if len(sys.argv) > 1:
-		param = sys.argv[1].lower()
-	else:
-		param = 'fetch'
+    param = None
+    if len(sys.argv) > 1:
+        param = sys.argv[1].lower()
+    else:
+        param = 'fetch'
 
-	if param == 'autoconf':
-		TorPlugin.autoconf()
-		sys.exit(0)
-	elif param == 'suggest':
-		TorPlugin.suggest()
-		sys.exit(0)
-	else:
-		# detect data provider
-		provider = None
-		if __file__.endswith('_connections'):
-			provider = TorConnections()
-		elif __file__.endswith('_traffic'):
-			provider = TorTraffic()
-		else:
-			print('Unknown plugin name, try "suggest" for a list of possible ones.')
-			sys.exit(0)
+    if param == 'autoconf':
+        TorPlugin.autoconf()
+        sys.exit(0)
+    elif param == 'suggest':
+        TorPlugin.suggest()
+        sys.exit(0)
+    else:
+        # detect data provider
+        provider = None
+        if __file__.endswith('_connections'):
+            provider = TorConnections()
+        elif __file__.endswith('_traffic'):
+            provider = TorTraffic()
+        else:
+            print('Unknown plugin name, try "suggest" for a list of possible ones.')
+            sys.exit(0)
 
-		if param == 'config':
-			provider.conf()
-		elif param == 'fetch':
-			provider.fetch()
-		else:
-			print('Unknown parameter "{}"'.format(param))
+        if param == 'config':
+            provider.conf()
+        elif param == 'fetch':
+            provider.fetch()
+        else:
+            print('Unknown parameter "{}"'.format(param))
