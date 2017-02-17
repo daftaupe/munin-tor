694cbcb5dcd251eb466fc2d3ef3e9b9133fdcc14
pat <pierre-alain.toret@protonmail.com>
2017-02-17 23:17:59 +0100

    Merge python scripts

--- tor_.py
+++ tor_.py
@@ -1,17 +1,55 @@
 #!/usr/bin/python2
 
 from __future__ import print_function
+from collections import Counter
+from stem import CircStatus
+from stem.control import Controller
+#from tor_ import TorPlugin, authenticate, gen_controller
 import os
 import sys
+import stem
+import stem.control
+import argparse
+import json
 
-try:
-    import stem
-    from stem.control import Controller
-except ImportError:
-    print('no (tor-munin requires the stem library from https://stem.torproject.org.)')
-    sys.exit()
 
-import circuits_by_country
+#try:
+#    import stem
+#    from stem.control import Controller
+#except ImportError:
+#    print('no (tor-munin requires the stem library from https://stem.torproject.org.)')
+#    sys.exit()
+
+
+DEFAULT_GEOIP_PATH = "/usr/share/GeoIP/GeoIP.dat"
+CACHE_FNAME = 'munin_tor_country_stats.json'
+
+
+def simplify(cn):
+    """Simplify country name"""
+    cn = cn.replace(' ', '_')
+    cn = cn.replace("'", '_')
+    cn = cn.split(',', 1)[0]
+    return cn
+
+def get_info(query, port=9051, socket='/var/run/tor/control', connect_method='port'):
+    if connect_method == 'port':
+        def gen_controller():
+            return stem.control.Controller.from_port(port=port)
+    elif connect_method == 'socket':
+        def gen_controller():
+            return stem.control.Controller.from_socket_file(path=socket)
+    else:
+        raise ValueError("env.connectmethod contains in invalid value. Please specify either 'port' or 'socket'.")
+
+    with gen_controller() as controller:
+        controller.authenticate()
+
+        try:
+            response = controller.get_info(query)
+            print(response)
+        except stem.InvalidArguments as e:
+            print("{}".format(e))
 
 '''
  Here be dragons
@@ -97,10 +135,12 @@ class TorPlugin(object):
 
     @staticmethod
     def suggest():
-        options = ['connections', 'traffic', 'dormant', 'bandwidth', 'flags']
-        tc = circuits_by_country.TorCountries()
-        if tc.available:
-            options.append('countries')
+        options = ['connections', 'traffic', 'dormant', 'bandwidth', 'flags', 'countries']
+        ##tc = circuits_by_country.TorCountries()
+	#TorCountries is always available now, because it is included in the tor_.py file
+        #tc = TorCountries()
+        #if tc.available:
+        #    options.append('countries')
 
         for option in options:
             print(option)
@@ -168,7 +208,8 @@ class TorDormant(TorPlugin):
     def fetch(self):
         with gen_controller() as controller:
             try:
-                controller.authenticate()
+                #controller.authenticate()
+                authenticate(controller)
 
                 response = controller.get_info('dormant', None)
                 if response is None:
@@ -295,6 +336,111 @@ class TorFlags(TorPlugin):
                 else:
                     print('{}.value 0'.format(flag))
 
+class TorCountries(TorPlugin):
+    def __init__(self):
+        # Configure plugin
+        self.cache_dir_name = os.environ.get('torcachedir', None)
+        if self.cache_dir_name is not None:
+            self.cache_dir_name = os.path.join(self.cache_dir_name,
+                                               CACHE_FNAME)
+
+        max_countries = os.environ.get('tormaxcountries', 15)
+        self.max_countries = int(max_countries)
+
+        geoip_path = os.environ.get('torgeoippath', DEFAULT_GEOIP_PATH)
+        try:
+            import GeoIP
+            self.geodb = GeoIP.open(geoip_path, GeoIP.GEOIP_MEMORY_CACHE)
+            self.available = True
+        except Exception:
+            self.available = False
+
+    def conf(self):
+        """Configure plugin"""
+        if not self.available:
+            return
+
+        graph = {'title': 'Countries',
+                 'args': '-l 0 --base 1000',
+                 'vlabel': 'countries',
+                 'category': 'Tor',
+                 'info': 'OR connections by state'}
+        labels = {}
+
+        countries_num = self.top_countries()
+
+        for c, v in countries_num:
+            labels[c] = {'label': c, 'min': 0, 'max': 25000, 'type': 'GAUGE'}
+
+        TorPlugin.conf_from_dict(graph, labels)
+
+        # If needed, create cache file at config time
+        if self.cache_dir_name:
+            with open(self.cache_dir_name, 'w') as f:
+                json.dump(countries_num, f)
+
+    def fetch(self):
+        """Generate metrics"""
+
+        # If possible, read cached data instead of doing the processing twice
+        if not self.available:
+            return
+
+        try:
+            with open(self.cache_dir_name) as f:
+                countries_num = json.load(f)
+        except:
+            # Fallback if cache_dir_name is not set, unreadable or any other
+            # error
+            countries_num = self.top_countries()
+
+        for c, v in countries_num:
+            print("%s.value %d" % (c, v))
+
+    # Unused
+    #@staticmethod
+    #def _gen_ipaddrs_from_circuits(controller):
+    #    """Generate a sequence of ipaddrs for every built circuit"""
+    #    # Currently unused
+    #    for circ in controller.get_circuits():
+    #        if circ.status != CircStatus.BUILT:
+    #            continue
+    #
+    #        for entry in circ.path:
+    #            fingerprint, nickname = entry
+    #
+    #            desc = controller.get_network_status(fingerprint, None)
+    #            if desc:
+    #                ipaddr = desc.address
+    #                yield ipaddr
+
+    @staticmethod
+    def _gen_ipaddrs_from_statuses(controller):
+        """Generate a sequence of ipaddrs for every network status"""
+        for desc in controller.get_network_statuses():
+            ipaddr = desc.address
+            yield ipaddr
+
+    def _gen_countries(self, controller):
+        """Generate a sequence of countries for every built circuit"""
+        for ipaddr in self._gen_ipaddrs_from_statuses(controller):
+            country = self.geodb.country_name_by_addr(ipaddr)
+            if country is None:
+                yield 'Unknown'
+                continue
+
+            yield simplify(country)
+
+    def top_countries(self):
+        """Build a list of top countries by number of circuits"""
+        with gen_controller() as controller:
+            try:
+                authenticate(controller)
+                c = Counter(self._gen_countries(controller))
+                return sorted(c.most_common(self.max_countries))
+            except stem.connection.AuthenticationFailure as e:
+                print('Authentication failed ({})'.format(e))
+                return []
 
 def main():
     if len(sys.argv) > 1:
@@ -317,7 +463,8 @@ def main():
         elif __file__.endswith('_traffic'):
             provider = TorTraffic()
         elif __file__.endswith('_countries'):
-            provider = circuits_by_country.TorCountries()
+            #provider = circuits_by_country.TorCountries()
+            provider = TorCountries()
         elif __file__.endswith('_bandwidth'):
             provider = TorBandwidth()
         elif __file__.endswith('_flags'):
@@ -335,3 +482,20 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+#if __name__ == "__main__":
+#    parser = argparse.ArgumentParser()
+#    parser.add_argument("query", type=str,
+#                        help='query parameter for get_info request')
+#    parser.add_argument("-p", "--port", type=int,
+#                        help='set the port used for connecting to tor'
+#                        ' (default: 9051)', default=9051)
+#    parser.add_argument("-s", "--socket", type=str,
+#                        help='set the path to the socket used for connecting to tor (if connectmethod=socket)'
+#                        ' (default: /var/run/tor/control)', default='/var/run/tor/control')
+#    parser.add_argument("-m", "--connectmethod", type=str,
+#                        help='the method used to connect to tor (port or socket)'
+#                        ' (default: port)', default='port')
+#    args = parser.parse_args()
+#
+#    get_info(args.query, args.port, args.socket, args.connectmethod)
